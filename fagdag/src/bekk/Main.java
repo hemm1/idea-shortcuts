ca59c4c2d76adbce94ff96f7a324e6bacc72d861
Peter Hemmen <peter.hemmen@gmail.com>
2013-02-24 20:46:42 +0100

    opprydding

--- fagdag/src/bekk/Main.java
+++ fagdag/src/bekk/Main.java
@@ -2,15 +2,9 @@ package bekk;
 
 import snake.Snake;
 
-import java.util.ArrayList;
-
 public class Main {
 
     public static void main(String[] args) {
-
-        ArrayList<String> strings = new ArrayList<String>();
-
-        OtherClass.sortList(strings);
 
         Snake snake = new Snake();
         snake.startMovement();
